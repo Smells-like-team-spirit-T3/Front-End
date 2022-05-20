536f6af6605ab91665cb52172ee1dec11c9fb64e
Truong Son Mai <sonmt.work00@gmail.com>
2022-05-20 13:52:10 +0200

    get form data

--- pages/createtrip.js
+++ pages/createtrip.js
@@ -1,6 +1,30 @@
 import { globalBody, mainPageBody } from './style';
+import { useState } from 'react';
 
 export default function CreateTrip() {
+
+    const [startDate, setStartDate] = useState('')
+    const [endDate, setEndDate] = useState('')
+    const [destination, setDestination] = useState('')
+
+    const handleConfirm = () => {
+        doStuff();
+        notify('Stuff is done.');
+        handleSubmit();
+        setOpen(false);
+    };
+
+
+    const submitTrip = (e) =>{
+        e.preventDefault()
+        console.log("SUBMIT...")
+        console.log(startDate)
+        console.log(endDate)
+        console.log(destination)
+        var notification = "Your trip to " + destination.toUpperCase() + " from " + startDate + " to " + endDate + ". \nDo you want to create new trip?"
+        alert(notification)
+    }
+
     return (
         <div className='container'>
 
@@ -10,15 +34,15 @@ export default function CreateTrip() {
                 </h1>
                 <br/>
             
-                <form>
-                    <label for="sdate">Departure</label>
-                    <input type="date" id="sdate" name="startdate"/>
+                <form onSubmit={(e) => submitTrip(e)}>
+                    <label htmlFor="sdate">Departure</label>
+                    <input type="date" id="sdate" name="startdate" onChange={(e) => setStartDate(e.target.value)}/>
 
-                    <label for="edate">Return</label>
-                    <input type="date" id="edate" name="enddate"/>
+                    <label htmlFor="edate">Return</label>
+                    <input type="date" id="edate" name="enddate" onChange={(e) => setEndDate(e.target.value)}/>
 
-                    <label for="country">Destination</label>
-                    <input type="text" id="dest" name="dest" list="cities"/>
+                    <label htmlFor="country">Destination</label>
+                    <input type="text" id="dest" name="dest" list="cities" onChange={(e) => setDestination(e.target.value)}/>
                     <datalist id="cities">
                         <option value="Paris">Paris</option>
                         <option value="Berlin">Berlin</option>
@@ -28,7 +52,7 @@ export default function CreateTrip() {
                         <option value="Basel">Basel</option>
                     </datalist>
 
-                    <input type="submit" value="Submit"/>
+                    <input type="submit" value="Submit"/> 
                 </form>
             </main>
 
