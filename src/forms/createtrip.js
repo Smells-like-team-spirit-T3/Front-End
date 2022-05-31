deee6e4c29eb948c5ff98862639c5583e7f3129b
Truong Son Mai <sonmt.work00@gmail.com>
2022-05-31 10:29:10 +0200

    merging branches without fixing import module error

--- src/forms/createtrip.js
+++ src/forms/createtrip.js
@@ -1,4 +1,4 @@
-import { globalBody, mainPageBody } from './style';
+// import { globalBody, mainPageBody } from './style';
 import { useState } from 'react';
 import { Dialog } from '@material-ui/core';
 import { DialogTitle } from '@material-ui/core';
@@ -7,7 +7,7 @@ import { DialogContentText } from '@material-ui/core';
 import { DialogActions } from '@material-ui/core';
 import { Button } from '@material-ui/core';
 import axios from 'axios';
-import './style.css'
+// import './style.css'
 
 export default function CreateTrip() {
 
